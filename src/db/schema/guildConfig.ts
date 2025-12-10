c4a14d1b01e6d61778f166dee5f4cedb4a4103f0
Katie Macke <katie@sleepysheepy.dev>
2025-12-09 22:45:40 -0500

    types are neat

--- src/db/schema/guildConfig.ts
+++ src/db/schema/guildConfig.ts
@@ -1,8 +1,9 @@
+import type { Snowflake } from 'discord.js'
 import { pgTable, text } from 'drizzle-orm/pg-core'
 import { createdAt, snowflake, updatedAt } from '@/db/schemaHelpers'
 
 export const GuildConfigTable = pgTable('guild', {
-  id: snowflake.primaryKey(),
+  id: snowflake.primaryKey().$type<Snowflake>(),
   createdAt,
   updatedAt,
 
