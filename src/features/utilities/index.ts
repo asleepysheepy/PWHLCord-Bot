217c9acf5c3378710dfb47e477967abc68bd093e
Katie Macke <katie@sleepysheepy.dev>
2025-12-11 00:09:49 -0500

    Remove Ready event

--- src/features/utilities/index.ts
+++ src/features/utilities/index.ts
@@ -1,5 +1,4 @@
 import { MessageCreateEvent } from '@/features/utilities/events/messageCreate'
-import { ReadyEvent } from '@/features/utilities/events/ready'
 import type { Event } from '@/models/event'
 
-export const events: Event[] = [MessageCreateEvent, ReadyEvent]
+export const events: Event[] = [MessageCreateEvent]
